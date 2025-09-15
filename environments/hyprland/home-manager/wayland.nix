23877ea441ff6c88588ff94842590fd0751af0b4
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-09-15 21:27:59 +0200

    remove deprecated config option

--- environments/hyprland/home-manager/wayland.nix
+++ environments/hyprland/home-manager/wayland.nix
@@ -185,10 +185,6 @@ in
             };
           };
 
-          gestures = {
-            workspace_swipe = false;
-          };
-
           windowrule = [
             "suppressevent maximize, class:.*"
             "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
