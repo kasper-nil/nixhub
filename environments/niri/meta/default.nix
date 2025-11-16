8b3738c9a2eeee9b2dc899e76d1aa71b3355f0c6
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-11-16 12:17:29 +0100

    attempt on refactoring the flake file for better clarity

---
+++ environments/niri/meta/default.nix
@@ -0,0 +1,38 @@
+{
+  catppuccin,
+  nixcord,
+  spicetify-nix,
+  ...
+}:
+{
+  description = "Niri desktop environment configuration.";
+
+  nixos = {
+    imports = [
+      ../config/modules
+      ../modules
+
+      catppuccin.nixosModules.catppuccin
+    ];
+  };
+
+  homeManager = {
+    imports = [
+      ../config/home-manager
+      ../home-manager
+
+      # Specific arguments or common modules if needed
+      catppuccin.homeModules.catppuccin
+      # spicetify-nix.homeManagerModules.default # Still commented out as per your original
+      nixcord.homeModules.nixcord
+    ];
+  };
+
+  # Arguments to pass specifically to Hyprland's Home Manager module
+  homeManagerModuleArgs = {
+    # inherit spicetify-nix;
+  };
+
+  # Any other metadata or specific configs for hyprland
+  extraHomeManagerModules = [ ]; # spicetify-nix for example
+}
--- flake.nix
+++ flake.nix
@@ -27,117 +27,99 @@
     {
       self,
       nixpkgs,
+      home-manager, # Still needed if Home Manager configurations are elsewhere and require `home-manager.lib`
       catppuccin,
       spicetify-nix,
       nixcord,
       ...
-    }:
-    {
-      nixosModules = {
-        hyprland =
-          { ... }:
-          {
-            imports = [
-              catppuccin.nixosModules.catppuccin
-              (import ./environments/hyprland/config/modules)
-              (import ./environments/hyprland/modules)
-            ];
-          };
-
-        niri =
-          { ... }:
-          {
-            imports = [
-              catppuccin.nixosModules.catppuccin
-              (import ./environments/niri/config/modules)
-              (import ./environments/niri/modules)
-            ];
-          };
+    }@inputs: # Capture all inputs into `inputs` for passing to meta files
+    let
+      # Define all environment metadata files
+      # Each meta file is a function that expects relevant inputs.
+      # We call it here, passing all available inputs from the flake.
+      environmentMeta = {
+        hyprland = (import ./environments/meta/hyprland.nix inputs);
+        niri = (import ./environments/meta/niri.nix inputs);
+        # Add more environments here as you create their meta files
+        # plasma = (import ./environments/meta/plasma.nix inputs);
       };
 
-      homeModules = {
-        hyprland =
-          { ... }:
-          {
-            _module.args = {
-              inherit spicetify-nix;
-            };
-            imports = [
-              catppuccin.homeModules.catppuccin
-              # spicetify-nix.homeManagerModules.default
-              nixcord.homeModules.nixcord
-              (import ./environments/hyprland/config/home-manager)
-              (import ./environments/hyprland/home-manager)
-            ];
-          };
-
-        niri =
-          { ... }:
-          {
-            _module.args = {
-              inherit spicetify-nix;
-            };
-            imports = [
-              catppuccin.homeModules.catppuccin
-              spicetify-nix.homeManagerModules.default
-              nixcord.homeModules.nixcord
-              (import ./environments/niri/config/home-manager)
-              (import ./environments/niri/home-manager)
-            ];
-          };
-      };
-      checks.x86_64-linux =
-        let
-          system = "x86_64-linux";
-          lib = nixpkgs.lib;
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+      # Function to create NixOS modules dynamically from environmentMeta
+      mkNixOSModule =
+        envName: envMeta:
+        { ... }:
         {
-          # Validate NixOS hyprland module (use the toplevel derivation)
-          hyprlandModule =
-            (lib.nixosSystem {
-              inherit system;
-              modules = [
-                self.nixosModules.hyprland
-                { users.users.test.isNormalUser = true; }
-              ];
-            }).config.system.build.toplevel;
-
-          # Validate NixOS niri module
-          niriModule =
-            (lib.nixosSystem {
-              inherit system;
-              modules = [
-                self.nixosModules.niri
-                { users.users.test.isNormalUser = true; }
-              ];
-            }).config.system.build.toplevel;
-
-          # Validate Home-Manager hyprland module (use the activation derivation)
-          hyprlandHome =
-            (lib.homeManagerConfiguration {
-              pkgs = pkgs;
-              modules = [
-                self.homeModules.hyprland
-                {
-                  home.username = "test";
-                  home.homeDirectory = "/home/test";
-                }
-              ];
-            }).activationPackage;
-
-          # Validate Home-Manager niri module
-          niriHome =
-            (lib.homeManagerConfiguration {
-              pkgs = pkgs;
-              modules = [
-                self.homeModules.niri
-                {
-                  home.username = "test";
-                  home.homeDirectory = "/home/test";
-                }
-              ];
-            }).activationPackage;
+          imports = envMeta.nixosModules;
         };
+
+      # Function to create Home Manager modules dynamically from environmentMeta
+      mkHomeManagerModule =
+        envName: envMeta:
+        { ... }:
+        {
+          _module.args = envMeta.homeManagerModuleArgs; # Use specific args from meta file
+          imports = envMeta.homeManagerModules;
+        };
+
+      # Generate the nixosModules and homeModules attribute sets
+      # by iterating over environmentMeta
+      nixosModules = builtins.mapAttrs mkNixOSModule environmentMeta;
+      homeModules = builtins.mapAttrs mkHomeManagerModule environmentMeta;
+    in
+    {
+      inherit nixosModules homeModules;
+      # checks.x86_64-linux =
+      #   let
+      #     system = "x86_64-linux";
+      #     lib = nixpkgs.lib;
+      #     pkgs = nixpkgs.legacyPackages.${system};
+      #   in
+      #   {
+      #     # Validate NixOS hyprland module (use the toplevel derivation)
+      #     hyprlandModule =
+      #       (lib.nixosSystem {
+      #         inherit system;
+      #         modules = [
+      #           self.nixosModules.hyprland
+      #           { users.users.test.isNormalUser = true; }
+      #         ];
+      #       }).config.system.build.toplevel;
+
+      #     # Validate NixOS niri module
+      #     niriModule =
+      #       (lib.nixosSystem {
+      #         inherit system;
+      #         modules = [
+      #           self.nixosModules.niri
+      #           { users.users.test.isNormalUser = true; }
+      #         ];
+      #       }).config.system.build.toplevel;
+
+      #     # Validate Home-Manager hyprland module (use the activation derivation)
+      #     hyprlandHome =
+      #       (lib.homeManagerConfiguration {
+      #         pkgs = pkgs;
+      #         modules = [
+      #           self.homeModules.hyprland
+      #           {
+      #             home.username = "test";
+      #             home.homeDirectory = "/home/test";
+      #           }
+      #         ];
+      #       }).activationPackage;
+
+      #     # Validate Home-Manager niri module
+      #     niriHome =
+      #       (lib.homeManagerConfiguration {
+      #         pkgs = pkgs;
+      #         modules = [
+      #           self.homeModules.niri
+      #           {
+      #             home.username = "test";
+      #             home.homeDirectory = "/home/test";
+      #           }
+      #         ];
+      #       }).activationPackage;
+      #   };
     };
 }
