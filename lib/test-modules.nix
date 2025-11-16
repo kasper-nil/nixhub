b3083532180b22585fefe7fd0f595d774a81fa9d
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-11-16 12:59:25 +0100

    rename the test modules file

---
+++ lib/test-modules.nix
@@ -0,0 +1,38 @@
+{
+  nixosTestModule =
+    { ... }:
+    {
+      # Essential settings to make a NixOS system buildable for tests
+      # without actually trying to boot it.
+      boot.loader.systemd-boot.enable = false;
+      boot.loader.efi.canTouchEfiVariables = false;
+      boot.loader.grub.enable = false;
+
+      # Dummy root filesystem to satisfy NixOS assertions
+      fileSystems."/" = {
+        device = "/dev/disk/by-label/NIXOS_BOOT";
+        fsType = "ext4";
+      };
+
+      networking.hostName = "test-nixos-system"; # Required
+      system.stateVersion = "23.11"; # Important to set
+
+      # A test user for basic setup
+      users.users.testuser = {
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "users"
+        ];
+      };
+    };
+
+  homeTestModule =
+    { ... }:
+    {
+      # Essential settings for a Home Manager configuration
+      home.username = "testuser";
+      home.homeDirectory = "/home/testuser";
+      home.stateVersion = "23.11";
+    };
+}
