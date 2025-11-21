9874c8cf5f4f9382e10d5ced30ffd37337beaf85
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-11-21 22:05:42 +0100

    disable config files for test

--- environments/hyprland/home-manager/xdg.nix
+++ environments/hyprland/home-manager/xdg.nix
@@ -8,15 +8,15 @@ let
 in
 {
   xdg = lib.mkIf cfg.enable {
-    configFile = {
-      "gtk-4.0/assets".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    # configFile = {
+    #   "gtk-4.0/assets".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
 
-      "gtk-4.0/gtk.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    #   "gtk-4.0/gtk.css".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
 
-      "gtk-4.0/gtk-dark.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-    };
+    #   "gtk-4.0/gtk-dark.css".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    # };
   };
 }
