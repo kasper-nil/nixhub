a633dac44292ce43d93d5cf25d39e7130ed36f94
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-11-21 20:58:09 +0100

    add gtk catppuccin theme

--- environments/hyprland/home-manager/gtk.nix
+++ environments/hyprland/home-manager/gtk.nix
@@ -1,9 +1,18 @@
-{ config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.nixhub.hyprland;
 in
 {
   gtk = lib.mkIf cfg.enable {
     enable = true;
+    theme = {
+      name = "catppuccin-mocha-lavender-standard+default";
+      package = pkgs.catppuccin-gtk;
+    };
   };
 }
