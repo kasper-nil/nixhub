2c1423259f383f78cdc9c703dc3c2756c19855d7
Kasper Nilssen <kasper.nilssen1@gmail.com>
2025-11-21 21:55:18 +0100

    add explicit gtk4 config

--- environments/hyprland/home-manager/xdg.nix
+++ environments/hyprland/home-manager/xdg.nix
@@ -1,5 +1,12 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
+  cfg = config.nixhub.hyprland;
+
   catppuccinGtk = pkgs.catppuccin-gtk.override {
     accents = [ "lavender" ];
     size = "standard";
@@ -10,11 +17,19 @@ let
   catppuccinName = "catppuccin-mocha-lavender-standard+default";
 in
 {
-  # Symlink gtk-4.0 assets from the theme package into ~/.config/gtk-4.0
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source =
-      "${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/gtk-dark.css";
+  # Explicit GTK4 config, bypassing HM’s generated hm_gtk4.0gtk.css
+  xdg.configFile = lib.mkIf cfg.enable {
+    "gtk-4.0/gtk.css".text = ''
+      @import url("file://${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/gtk.css");
+    '';
+
+    "gtk-4.0/gtk-dark.css".text = ''
+      @import url("file://${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/gtk-dark.css");
+    '';
+
+    "gtk-4.0/assets" = {
+      recursive = true;
+      source = "${catppuccinGtk}/share/themes/${catppuccinName}/gtk-4.0/assets";
+    };
   };
 }
